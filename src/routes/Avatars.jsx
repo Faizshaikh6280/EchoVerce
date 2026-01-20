c4a90e71ff37a25b4501308b7ee7c5a3efb448d0
Faiz Alam <faizalam2432005@gmail.com>
2026-01-20 18:14:16 +0530

    done

--- src/routes/Avatars.jsx
+++ src/routes/Avatars.jsx
@@ -18,7 +18,7 @@ const INITIAL_DATA = [
     name: "SHINCHAN",
     category: "Funny",
     isPaid: false,
-    image: "/images/shinchan.png",
+    image: "/images/Shinchan.png",
     slug: "shinchan",
   },
   {
